c1fe79887fae610a3a3621982eeef622c0433628
Aram47 <aram.minasyan.00@gmail.com>
2025-02-15 22:33:25 +0400

    hello

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { FileModule } from './file/file.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UserModule, ConfigModule.forRoot({})],
+  imports: [PrismaModule, AuthModule, UserModule, ConfigModule.forRoot({}), FileModule],
   controllers: [AppController],
   providers: [AppService],
 })
