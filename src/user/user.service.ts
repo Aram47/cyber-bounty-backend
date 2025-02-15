dd721d9cdcb956d32d1d5e5aeff57bd3afdb024d
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2025-02-16 02:49:50 +0400

    Handled merge conflicts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,8 +4,6 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Prisma, User } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
-// import { PrismaService } from 'src/prisma/prisma.service';
-// import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UserService {
