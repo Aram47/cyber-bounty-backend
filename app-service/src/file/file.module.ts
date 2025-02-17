824d03eb9b55a641bcb594e9353e84075a13e062
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2025-02-17 20:49:08 +0400

    fixed bug

--- app-service/src/file/file.module.ts
+++ app-service/src/file/file.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FileService } from './file.service';
 import { FileController } from './file.controller';
+import { UserModule } from '../user/user.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [UserModule, AuthModule],
   controllers: [FileController],
   providers: [FileService],
 })
